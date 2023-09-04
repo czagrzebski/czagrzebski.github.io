a1bc9c9eb9e2ac4eddbae0fa4547393b61f95623
Creed Zagrzbebski <czagrzebski@DESKTOP-4JOT5NT>
2023-09-04 13:59:34 -0500

    Added globe

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
-import { FaGraduationCap, FaGithub, FaLinkedin, FaFileAlt } from "react-icons/fa";
+import {
+    FaGraduationCap,
+    FaGithub,
+    FaLinkedin,
+    FaFileAlt,
+} from "react-icons/fa";
 import { HiLocationMarker } from "react-icons/hi";
 import { socialLinks } from "config";
+import Globe from "react-globe.gl";
 
 const Container = styled.section`
     display: flex;
-    box-sizing: border-box;
     justify-content: center;
     align-items: center;
-    min-height: 100%;
     flex: 1;
-    flex-direction: column;
+    box-sizing: border-box;
     min-height: 100vh;
     width: 100%;
     overflow-x: hidden;
+    gap: 50px;
 
-    color: ${({ theme }) => theme.palette.primaryColorLightest}; 
-    
+    color: ${({ theme }) => theme.palette.primaryColorLightest};
+
     padding-left: 400px;
     padding-right: 300px;
 
@@ -30,13 +35,12 @@ const Container = styled.section`
     }
 
     overflow: auto;
-
 `;
 
 const SocialButtons = styled.div`
     display: flex;
     gap: 20px;
-  
+
     button {
         background-color: transparent;
         border: 2px solid ${({ theme }) => theme.palette.primaryColorLightest};
@@ -47,16 +51,16 @@ const SocialButtons = styled.div`
 
         &:hover {
             color: ${({ theme }) => theme.palette.secondaryColorPrimary};
-            border: 2px solid ${({ theme }) => theme.palette.secondaryColorPrimary};
-            background-color: ${({ theme }) => theme.palette.hoverSecondaryColor};
+            border: 2px solid
+                ${({ theme }) => theme.palette.secondaryColorPrimary};
+            background-color: ${({ theme }) =>
+                theme.palette.hoverSecondaryColor};
             cursor: pointer;
-          
         }
-        
     }
 `;
 
-const Text  = styled.div`
+const Introduction = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
@@ -71,41 +75,96 @@ const Text  = styled.div`
     h1 {
         margin: 0;
     }
-
-
 `;
 
-
-const PlaceHolder = styled.div`;
-
+const GlobeDiv = styled.div`;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 400px;
+    width: 400px;
 `;
-
 
 const Hero = () => {
+    const globeEl = useRef()
+
+    useEffect(() => {
+        const globeInstance = globeEl.current
+        globeInstance.controls().autoRotate = true
+        globeInstance.controls().autoRotateSpeed = 2.5
+
+        // disable scroll zoom
+        globeInstance.controls().enableZoom = false
+
+        // Disable click and drag
+        globeInstance.controls().enablePan = false
+
+        // Set Initial Globe Position
+        globeInstance.pointOfView({ lat: 44.5236, lng: -89.5746, altitude: 2 }, 0)
+    }, [])
     return (
         <Container>
-            <Text>
+            <Introduction>
                 <h1> Hi, I'm Creed Zagrzebski. </h1>
-                <p>I am a passionate student and developer interested in Software Engineering, Computer Science, and Robotics. I enjoy learning new technologies and applying them to create real-world applications.</p>
                 <p>
-                    < HiLocationMarker  /> Wisconsin, USA
+                    I am a passionate student and developer interested in
+                    Software Engineering, Computer Science, and Robotics. I
+                    enjoy learning new technologies and applying them to create
+                    real-world applications.
                 </p>
                 <p>
-                    <FaGraduationCap /> Computer Science Student at the University of Wisconsin-La Crosse
+                    <HiLocationMarker /> Stevens Point, Wisconsin
+                </p>
+                <p>
+                    <FaGraduationCap /> Computer Science Student at the
+                    University of Wisconsin-La Crosse
                 </p>
                 <SocialButtons>
-                    <a href={socialLinks.github} target="_blank" rel="noreferrer"><button><FaGithub/> Github</button></a>
-                    <a href={socialLinks.linkedin} target="_blank" rel="noreferrer"><button><FaLinkedin/> LinkedIn</button></a>
-                    <a href={socialLinks.resume} target="_blank" rel="noreferrer"><button><FaFileAlt/> Resume</button></a>
+                    <a
+                        href={socialLinks.github}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <button>
+                            <FaGithub /> Github
+                        </button>
+                    </a>
+                    <a
+                        href={socialLinks.linkedin}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <button>
+                            <FaLinkedin /> LinkedIn
+                        </button>
+                    </a>
+                    <a
+                        href={socialLinks.resume}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <button>
+                            <FaFileAlt /> Resume
+                        </button>
+                    </a>
                 </SocialButtons>
-            </Text>
-            <PlaceHolder>
-            
-            </PlaceHolder>
+            </Introduction>
+            <GlobeDiv>
+                <Globe
+                    ref={globeEl}
+                    globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                    backgroundColor="rgba(0,0,0,0)"
+                    width={600}
+                    height={600}
+                    labelsData={[
+                        { lat: 44.5236, lng: -89.5746, text: "Stevens Point, Wisconsin"},
+                    ]}
+                    labelSize={2}
+                    labelDotRadius={1}
+                />
+            </GlobeDiv>
         </Container>
-    )
-}
+    );
+};
 
 export default Hero;
-
-
